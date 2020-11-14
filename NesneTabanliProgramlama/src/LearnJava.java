9bd39c319af5ac530fab82e7c26eb28ba4f3a921
90536 <90536@DESKTOP-SG7E860.local>
2020-11-14 12:34:31 +0300

    Constructor Denemesi

--- NesneTabanliProgramlama/src/LearnJava.java
+++ NesneTabanliProgramlama/src/LearnJava.java
@@ -1,12 +1,23 @@
 
 public class LearnJava {
 
+	public String denemeFonksiyon() {
+		String a = "deneme";
+		return a;
+	}
+	
+	public LearnJava() {
+		System.out.println(denemeFonksiyon());
+	}
+	
 	public static void main(String[] args) {
+		LearnJava nesne = new LearnJava();
+		
+		
 		int a = 4;
 		String ad = "yusuf";
 		System.out.println(a);
 		System.out.println(ad.length());
-		
 		
 	}
 
