afd5a1add4a12fadf220a47fdcf9732443cfdad5
90536 <90536@DESKTOP-SG7E860.local>
2020-11-14 12:14:16 +0300

    Nesne Tabanlı Programlama

---
+++ NesneTabanliProgramlama/src/LearnJava.java
@@ -0,0 +1,13 @@
+
+public class LearnJava {
+
+	public static void main(String[] args) {
+		int a = 4;
+		String ad = "yusuf";
+		System.out.println(a);
+		System.out.println(ad.length());
+		
+		
+	}
+
+}
